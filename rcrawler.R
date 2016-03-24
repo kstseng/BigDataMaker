a9b5561e3d4725c05c7df9819f56e25dd16ddc17
kstseng <ddd7975@gmail.com>
2016-03-24 21:44:54 +0800

    data frame

--- rcrawler.R
+++ rcrawler.R
@@ -1,4 +1,4 @@
-install.packages("rvest")
+# install.packages("rvest")
 library(rvest)
 
 #---
@@ -18,10 +18,26 @@ time <- rtddt %>% html_nodes('time') %>% html_text()
 title <- rtddt %>% html_node('h1')  %>% html_text() %>% iconv('UTF-8', 'UTF-8')
 category <- rtddt %>% html_node('h2')  %>% html_text() %>% iconv('UTF-8', 'UTF-8')
 
-applenews <- data.frame(time=time, title = title, category = category)
+#---
+domain <- "http://www.appledaily.com.tw/"
+url <- rtddt %>% html_node('a') %>% html_attr('href')
+url <- gsub(domain, "", url) # global substitution, because url[4] include domain name
+url <- paste0(domain, url)
 
+#--- add link
+applenews <- data.frame(time=time, title = title, category = category, url = url, stringsAsFactors = F)
 
+#---
+applenews$content <- sapply(applenews$url, function(e){
+  read_html(e) %>% html_node('.trans') %>% html_text() %>% iconv('UTF-8', 'UTF-8')
+})
 
-
-
+#---
+# install.packages("jiebaR")
+library(jiebaR)
+mixseg = worker()
+segment(code=as.character(applenews$title[1]), jiebar = mixseg)
+word <- unlist(sapply(applenews$title, function(e){
+  segment(code=as.character(e), jiebar = mixseg)
+}))
 
